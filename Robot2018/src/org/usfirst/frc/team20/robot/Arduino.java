ffc757a007ad0f4017127624d65277302e8b62fd
Andrew S <yipten@outlook.com>
2018-02-19 18:11:36 -0500

    2/19/2018 update Arduino.java
    Changed so that while elevator is moving, 29 is not sent but just a value 2 to 20. Also deleted code for reading from the IR sensor.

--- Robot2018/src/org/usfirst/frc/team20/robot/Arduino.java
+++ Robot2018/src/org/usfirst/frc/team20/robot/Arduino.java
@@ -5,34 +5,12 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.I2C.Port;
 
 public class Arduino {
-	I2C Wire;									//initializes I2C communication on port 1
+	I2C Wire;	//initializes I2C communication on port 1
 	Zenith ob;
-	private final int numOfBytes = 1;					//number of bytes stored in sensorData
-	private byte[] sensorData = new byte[numOfBytes];	//all data from sensors is stored here
-	private byte IRSensor;	//data from IR sensor
 
 	Arduino(int port, Zenith o) {
 		Wire = new I2C(Port.kOnboard, port);
 		ob = o;
-	}
-	
-	/**
-	 * get array of data from Arduino and sets each variable to the correct value
-	 */
-	public void getSensorData() {
-		Wire.read(1, numOfBytes, sensorData);
-		IRSensor = sensorData[0];
-	}
-	
-	/**
-	 * checks for the presence of an object in front of the IR sensor
-	 * @return true if there is an object in front of the IR sensor
-	 */
-	public boolean getIRSensor() {
-		if (IRSensor == 1)
-			return true;
-		else
-			return false;
 	}
 	
 	/**
@@ -42,6 +20,7 @@ public class Arduino {
 	public void write(byte[] writeData) {
 		Wire.writeBulk(writeData);
 	}
+
 	/**
 	 * @param redAlliance: are we on the red alliance?
 	 * @param cube: do we have a cube?
@@ -61,8 +40,6 @@ public class Arduino {
 		} else if (cube) {
 			send[0] = 25;
 		} else if (elevatorMoving){
-			send[0] = 29;
-			write(send);
 			send[0] = (byte)((ob.elevatorMaster.getSelectedSensorPosition(0)/Constants.ELEVATOR_MAX_POSITION)*20);
 		} else if (alliance == Alliance.Red){
 			send[0] = 23;
